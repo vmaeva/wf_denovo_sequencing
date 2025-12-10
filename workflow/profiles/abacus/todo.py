8f9a27cf6e492d3000d6cb045e50d576f1bb7a32
vmaeva <maeva.veyssiere@gmail.com>
2025-12-10 14:33:35 +0100

    Update todo.py

--- workflow/profiles/abacus/todo.py
+++ workflow/profiles/abacus/todo.py
@@ -9,13 +9,18 @@ SEQS.append(DenovoSequencing(project="INI287",
                             genome='GRCh38.p14',
                             rnaseq= '/mnt/raid0/data_for_denovo_IP/INI287/unique_RNA_PDX159Aligned_TE.fa'
                             ))
+SEQS.append(DenovoSequencing(project="INI305", 
+                            MS_data="/mnt/beegfs/home/mveyssie/stagein/home/mveyssie/Documents/Projects/immuno_rhabdoid/data", 
+                            genome="GRCh38.p14",
+                            rnaseq= "/mnt/beegfs/home/mveyssie/stagein/home/mveyssie/Documents/Projects/immuno_rhabdoid/data/PDX209_RNA_TE.fa"
+                            ))                            
 '''
 
-SEQS.append(DenovoSequencing(project="INI305", 
-                             MS_data="/mnt/beegfs/home/mveyssie/stagein/home/mveyssie/Documents/Projects/immuno_rhabdoid/data", 
-                            genome="GRCh38.p14",
-                            #model=<name of the Casanovo model you want to use>, #optional, default is casanovo_nontryptic.ckpt
-                            rnaseq= "/mnt/beegfs/home/mveyssie/stagein/home/mveyssie/Documents/Projects/immuno_rhabdoid/data/PDX209_RNA_TE.fa", # optional, Fasta file containing extra transcript sequences (will be 3-frame translated!)!
-                            #extra= <path to the DNAseq data>, #optional, Fasta file containing extra sequences (DNA will be 6-frame translated!)!
-                            #var=<path to the var file> #optional, File containing variants (each line); could also be in ${input%.csv}.var, in which case you do not have to specify this parameter!
+SEQS.append(DenovoSequencing(project="INI305",
+                             MS_data=<path to MS data>, 
+                             genome="GRCh38.p14",
+                             #model=<name of the Casanovo model you want to use>, #optional, default is casanovo_nontryptic.ckpt
+                             #rnaseq= <path to the RNAseq data>, # optional, Fasta file containing extra transcript sequences (will be 3-frame translated!)!
+                             #extra= <path to the DNAseq data>, #optional, Fasta file containing extra sequences (DNA will be 6-frame translated!)!
+                             #var=<path to the var file> #optional, File containing variants (each line); could also be in ${input%.csv}.var, in which case you do not have to specify this parameter!
                             ))
